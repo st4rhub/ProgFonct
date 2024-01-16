55d9b16a50338e01bb0dc4abb92477e7c88493a3
st4rhub <oceanepioche@gmail.com>
2024-01-16 10:45:27 +0100

    comments

--- src/fordfulkerson.ml
+++ src/fordfulkerson.ml
@@ -22,7 +22,6 @@ let min_ecart list_arc =
 ;;
 
 
-
 (*adds n to an arc's flow /subtract n to the arc going from id1 to id2, add n from id2 to id1/*)
 let add_flow gr id1 id2 n = 
   let graph_aux = add_arc gr id1 id2 (-n) in 
@@ -46,6 +45,7 @@ let isCorrectArc sourceid precedingPath arc =
 ;;
 
 
+(* Finds an augmenting path, given a graph, a source node and a sink node *)
 let findAugmentingPath myGraph sourceid sinkid = 	
 
 	let rec findAugmentingPath_rec sourceid2 precedingPath =
@@ -53,38 +53,36 @@ let findAugmentingPath myGraph sourceid sinkid =
 		(*Function to test all arcs that are proven to be correct*)
 		let rec tester_tous_les_arcs arclist =
 			match arclist with
-					| [] -> raise Not_found   					(*If the list is empty we cannot find a path*)
-					(*Else we try to find paths for all correct edges 
-					and we stop when we have found a correct path*)
+					(*If the list is empty we cannot find a path*)
+					| [] -> raise Not_found   					
+					(*Else we try to find a path for the first arc of the list*)
 					| arc::rest -> 
 						begin
 							try
 								findAugmentingPath_rec arc.tgt (arc::precedingPath)
+								(* And if we can't find one, we try to find paths for the arcs in the remaining list *)
 							with
 								| Not_found -> tester_tous_les_arcs rest
 						end
 		in
-
 		
 		match (sourceid2,sinkid) with
-		| (a,b) when a=b -> precedingPath   (*If source=sink, we found path and we return it*)
+		| (a,b) when a=b -> precedingPath   (*If source = sink, we found an augmenting path and we return it*)
 		| _ -> 
-			(*Else we search all correct edges (non null and non already in the path) 
-			amongst all those starting from source*)
+			(*Else we search all correct edges (non null and not already in the path) amongst all those starting from source*)
 			begin						
 				let arcsCorrects = List.filter (isCorrectArc sourceid2 precedingPath) (out_arcs myGraph sourceid2) in	
 				tester_tous_les_arcs arcsCorrects		
 			end
 	in
 
-	
 
 let finalpath_reversed = findAugmentingPath_rec sourceid [] in 
 List.rev finalpath_reversed 
 ;;
 
 
-
+(* Implements the Ford Fulkerson algorithm *)
 let fordfulkerson originalGraph sourceid sinkid = 
 	let rec inner_ford_fulk gr source sink out_flow =
 		try
@@ -101,13 +99,17 @@ let fordfulkerson originalGraph sourceid sinkid =
 	inner_ford_fulk originalGraph sourceid sinkid 0
 ;;
 
-
+(* Transforms a deviation graph into a regular flow graph *)
 let flowgraph_from_ecart gr_init gr_ecart =
 	let f graph a = 
+		(* Find the corresponding arc in gr_ecart, given the arc a in a graph *)
 		let ecart_value = (Option.get (find_arc gr_ecart a.src a.tgt)).lbl in
+		(* If the value is greater than the capacity *)
 		if a.lbl < ecart_value then 
+			(* The flow becomes zero *)
 			add_arc graph a.src a.tgt (-a.lbl) 
 		else
+			(* Else, we substract the flow from the original capacity *)
 			add_arc graph a.src a.tgt (-ecart_value)
 		in
 	e_fold gr_init f gr_init
--- src/tools.ml
+++ src/tools.ml
@@ -4,12 +4,13 @@ open Graph
 
 let clone_nodes gr = n_fold gr (fun graph id -> new_node graph id) empty_graph;;
 
-let gmap gr f = (*f prend a rend b*)
-  let f1 gr a = new_arc gr {src=a.src;tgt=a.tgt;lbl=(f a.lbl)}   in   (*prend b graphe et a arc et rend b graph*)
+let gmap gr f =
+  (* Replaces an arc a's label by f(a) *)
+  let f1 gr a = new_arc gr {src=a.src;tgt=a.tgt;lbl=(f a.lbl)}   in 
   e_fold gr f1 (clone_nodes gr);;
 
 let add_arc gr id1 id2 n = 
   let a = find_arc gr id1 id2 in (*looks for the arc*)
-  match a with  (*if it's not there, creates it*)
+  match a with  (* if it doesn't exist, creates it *)
   |None -> new_arc gr {src=id1; tgt=id2; lbl=n}
-  |Some arc -> new_arc gr {src=id1; tgt=id2; lbl=(n+arc.lbl)};; (*if it's there, switches it*)
+  |Some arc -> new_arc gr {src=id1; tgt=id2; lbl=(n+arc.lbl)};; (* if does, adds n to the label *)
